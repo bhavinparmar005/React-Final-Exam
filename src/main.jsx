945fcbfa1af61e031415a502346d4c10e9ed5ece
bhavinparmar005 <bhavinparmar005@gmail.com>
2025-04-23 17:35:42 +0530

    hello

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,16 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import './bootstrap/dist/css/bootstrap.min.css'
-import './bootstrap/dist/js/bootstrap.bundle.min.js'
-import './bootstrap-icons/font/bootstrap-icons.css'
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle.js"
+// import "../node_modules/bootstrap-icons/font/bootstrap-icons."
+
+
+
+
+// import './bootstrap/dist/css/bootstrap.min.css'
+// import './bootstrap/dist/js/bootstrap.min.js'
+// import './bootstrap-icons/font/bootstrap-icons.css'
 import { Provider } from 'react-redux'
 import { store } from "../Store.jsx"
 
