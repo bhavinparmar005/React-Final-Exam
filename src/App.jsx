3475b36d7867f58f5f0c3576d03dd395775ba422
bhavinparmar005 <bhavinparmar005@gmail.com>
2025-04-23 18:38:00 +0530

    j

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './Pages/Home'
 import  Addblog from './Pages/Addblog'
-import  Editblog from './Pages/Editblog'
+import Editblog from './Pages/Editblog'
+
 import AdminLogin from './Pages/AdminLogin'
 import AdminSignup from './Pages/AdminSignup'
 import AdminPage from './Pages/AdminPage'
@@ -15,7 +16,7 @@ const App = () => {
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/add' element={<Addblog />} />
-          <Route path='/edit' element={<Editblog />} />
+          <Route path='/edit' element={<Editblog/>} />
           <Route path='/login' element={<AdminLogin />} />
           <Route path='/signup' element={<AdminSignup />} />
           <Route path='/adminpage' element={<AdminPage/>} />
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,9 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
-import "../node_modules/bootstrap/dist/js/bootstrap.bundle.js"
-import '../node_modules/bootstrap-icons/font/bootstrap-icons.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import 'bootstrap-icons/font/bootstrap-icons.css'
 import { Provider } from 'react-redux'
 import { store } from "../Store.jsx"
 
@@ -13,6 +13,4 @@ createRoot(document.getElementById('root')).render(
    <Provider store={store}>
       <App />
    </Provider>
-
-
 )
