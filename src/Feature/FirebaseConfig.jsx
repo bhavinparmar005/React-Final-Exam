a414a6ff13c95ae18c8a4d5a06583cd0c122f27f
bhavinparmar005 <bhavinparmar005@gmail.com>
2025-07-01 17:15:07 +0530

    restore firebase

--- src/Feature/FirebaseConfig.jsx
+++ src/Feature/FirebaseConfig.jsx
@@ -1,17 +1,38 @@
-// Import the functions you need from the SDKs you need
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getFirestore } from "firebase/firestore";
+
+// const firebaseConfig = {
+//     apiKey: "AIzaSyD199GxBO9pBKJnTcId8slOjCQ45SXdaOs",
+//     authDomain: "react-blog-page-7233d.firebaseapp.com",
+//     projectId: "react-blog-page-7233d",
+//     storageBucket: "react-blog-page-7233d.firebasestorage.app",
+//     messagingSenderId: "383656267314",
+//     appId: "1:383656267314:web:659028498b8c81f325762b",
+//     measurementId: "G-9CW9M5H1YH"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// export const db = getFirestore(app);
+
+
+
+
+
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyD199GxBO9pBKJnTcId8slOjCQ45SXdaOs",
-    authDomain: "react-blog-page-7233d.firebaseapp.com",
-    projectId: "react-blog-page-7233d",
-    storageBucket: "react-blog-page-7233d.firebasestorage.app",
-    messagingSenderId: "383656267314",
-    appId: "1:383656267314:web:659028498b8c81f325762b",
-    measurementId: "G-9CW9M5H1YH"
+  apiKey: "AIzaSyBd2ZY7HrB1UVBeO4x8DrYwTmskzzBKDB0",
+  authDomain: "blogpage-de244.firebaseapp.com",
+  projectId: "blogpage-de244",
+  storageBucket: "blogpage-de244.firebasestorage.app",
+  messagingSenderId: "791904299205",
+  appId: "1:791904299205:web:01aeb48a1aebbb333da8ac",
+  measurementId: "G-KCY7C6RV80"
 };
 
-// Initialize Firebase
+
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
